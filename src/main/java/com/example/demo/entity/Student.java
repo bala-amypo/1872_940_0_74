dfce0f22f3ac180ef0bee2655fe6264a99a4163d
bala-amypo <bala@amypo.in>
2025-12-17 05:26:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,7 +1,5 @@
 package com.example.demo.entity;
 
-
-
 import java.time.LocalDate;
 
 public class Student {
@@ -13,86 +11,28 @@ public class Student {
             return id;
         }
         public void setId(int id) {
-                                            this.id = id;
+            this.id = id;
+        }
 
-                                }
+        public String getName() {
+            return name;
+        }
 
-                                    public String getName() {
+        public void setName(String name) {
+            this.name = name;
+        }
 
-                                                return name;
+        public String getEmail() {
+            return email;
+        }
+        
+        public void setEmail(String email) {
+            this.email = email;
+        }
 
-                                    }
-
-                                        public void setName(String name) {
-
-                                                    this.name = name;
-
-                                        }
-
-                                            public String getEmail() {
-
-                                                        return email;
-
-                                            }
-
-                                                public void setEmail(String email) {
-
-                                                            this.email = email;
-
-                                                }
-
-                                                    public LocalDate getDob() {
-
-                                                                return dob;
-
-                                                    }
-
-                                                        public void setDob(LocalDate dob) {
-
-                                                                    this.dob = dob;
-
-                                                        }
-
-                                                            public float getCgpa() {
-
-                                                                        return cgpa;
-
-                                                            }
-
-                                                                public void setCgpa(float cgpa) {
-
-                                                                            this.cgpa = cgpa;
-
-                                                                }
-
-                                                                    public Student(int id, String name, String email, LocalDate dob, float cgpa) {
-
-                                                                                this.id = id;
-
-                                                                                        this.name = name;
-
-                                                                                                this.email = email;
-
-                                                                                                        this.dob = dob;
-
-                                                                                                                this.cgpa = cgpa;
-
-                                                                    }
-
-                                                                        public Student() {
-
-                                                                        }
-
-
-
-                                                                            // public void setId(int id) { //Setting the data Id
-                                                                            }
-                                                                                //     this.id = id;
-
-                                                                                    // }
-
-                                                                                        // public int getId() { //Getting the data Id
-                                                                                        }
-                                                                                            //     return id;
-
-                                                                                                // }
+        public Student(int id, String name, String email) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+        }
+}                                                                 // }
