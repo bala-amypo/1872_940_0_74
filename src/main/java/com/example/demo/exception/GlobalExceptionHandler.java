3fd98a63198876604c3ac1185d6d22f55a9f104f
bala-amypo <bala@amypo.in>
2025-12-18 09:00:11 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,4 +1,5 @@
 package com.example.demo.exception;
+
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.http.ResponseEntity;
@@ -6,24 +7,25 @@ import java.util.Map;
 import java.util.HashMap;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.FieldError;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler{
+public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class) //runtime expection
-        public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex){
-            return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
-        }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String,String>> handleMethod(MethodArgumentNotValidException mex){
-        
-        Map<String,String> error=new HashMap<>();
-
-        mex.getBindingResult().getFieldErrors().forEach(error ->
-                error.put(error.getField(), error.getDefaultMessage())
-        );
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(ResourceNotFoundException.class) //runtime exception
+    public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethod(MethodArgumentNotValidException mex){
+        
+        Map<String, String> error = new HashMap<>();
+
+        mex.getBindingResult().getFieldErrors().forEach(fieldError -> 
+            error.put(fieldError.getField(), fieldError.getDefaultMessage())
+        );
+        
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
