52a005168c511194b899611d60c743cba84a30fa
bala-amypo <bala@amypo.in>
2025-12-18 08:20:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.demo.exception; 
+import org.springframework.web.bind.annotation.RestControllerAdvice; 
+import org.springframework.web.bind.annotation.ExceptionHandler; 
+import org.springframework.http.ResponseEntity; 
+import org.springframework.http.HttpStatus; 
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+@RestControllerAdvice 
+public class GlobalExceptionHandler{ 
+    @ExceptionHandler(ResourceNotFoundException.class) 
+    public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex){ 
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND); 
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleMethod(MethodArgumentNotValidException mex){
+        return 
+    }
+}
