c95af62295419e6a908e7e04ad623211d7bd7c14
bala-amypo <bala@amypo.in>
2025-12-18 08:45:11 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -20,8 +20,8 @@ public class GlobalExceptionHandler{
         
         Map<String,String> error=new HashMap<>();
 
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-                errors.put(error.getField(), error.getDefaultMessage())
+        mex.getBindingResult().getFieldErrors().forEach(error ->
+                error.put(error.getField(), error.getDefaultMessage())
         );
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
