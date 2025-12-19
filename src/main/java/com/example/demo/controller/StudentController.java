9d5ac0ebd45beb93f3b2e18ac4e63348f6707c13
bala-amypo <bala@amypo.in>
2025-12-19 04:38:46 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,11 +1,11 @@
     package com.example.demo.controller;
-
     import org.springframework.beans.factory.annotation.Autowired;
     import org.springframework.web.bind.annotation.PostMapping;
     import org.springframework.web.bind.annotation.RequestBody;
     import org.springframework.web.bind.annotation.RequestMapping;
     import org.springframework.web.bind.annotation.GetMapping;
     import org.springframework.web.bind.annotation.RestController;
+    import org.springframework.web.bind.annotation.PutMapping;
     import org.springframework.http.ResponseEntity;
     import com.example.demo.entity.Student;
     import com.example.demo.service.StudentService;
@@ -14,7 +14,7 @@
     import org.springframework.web.bind.annotation.PathVariable;
 
     @RestController
-    @RequestMapping("/Studentsdata")
+    @RequestMapping("/Studentsdata")//for base url
     public class StudentController {
         @Autowired
         StudentService studentService;
@@ -28,5 +28,8 @@
         public Student getdata(@PathVariable Long id){
             return studentService.getStudentById(id);
         }
+
+        @PutMapping("/updatedata")
+        public Student updatedStudent(@PathVariable Long id, )
 
     }
